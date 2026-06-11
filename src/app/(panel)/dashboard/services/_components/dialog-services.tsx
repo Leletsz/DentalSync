8ebb56752d6dd9133a0d846e0a308b648b9b8f6b
devlets <leandrovictor2011@gmail.com>
2026-06-10 21:44:27 -0300

    cadastro do serviço

--- src/app/(panel)/dashboard/services/_components/dialog-services.tsx
+++ src/app/(panel)/dashboard/services/_components/dialog-services.tsx
@@ -17,13 +17,45 @@ import {
 } from "@/components/ui/field";
 
 import React from "react";
-import { UseDialogServiceForm } from "./dialog-service-form";
+import {
+  DialogServiceFormData,
+  UseDialogServiceForm,
+} from "./dialog-service-form";
 import { Controller } from "react-hook-form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { convertRealToCentes } from "@/utils/convertCurrency";
+import { createNewService } from "../_actions/create-service";
 
 export default function DialogService() {
   const form = UseDialogServiceForm();
+
+  async function onSubmit(values: DialogServiceFormData) {
+    const priceInCents = convertRealToCentes(values.price);
+    const hours = parseInt(values.hours) || 0;
+    const minutes = parseInt(values.minutes) || 0;
+
+    const duration = hours * 60 + minutes;
+    const response = await createNewService({
+      name: values.name,
+      price: priceInCents,
+      duration: duration,
+    });
+    console.log(response);
+  }
+
+  function changeCurrency(event: React.ChangeEvent<HTMLInputElement>) {
+    let { value } = event?.target;
+    value = value.replace(/\D/g, "");
+
+    if (value) {
+      value = (parseInt(value, 10) / 100).toFixed(2);
+      value = value.replace(".", ",");
+      value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+      event.target.value = value;
+      form.setValue("price", value);
+    }
+  }
   return (
     <div>
       <DialogHeader>
@@ -31,7 +63,7 @@ export default function DialogService() {
         <DialogDescription>Adicione um novo serviço</DialogDescription>
       </DialogHeader>
       <div>
-        <form>
+        <form className="space-y-2" onSubmit={form.handleSubmit(onSubmit)}>
           <FieldGroup className="flex flex-col gap-2">
             <Controller
               control={form.control}
@@ -52,7 +84,11 @@ export default function DialogService() {
                 <Field className="my-2">
                   <FieldLabel>Valor do serviço: </FieldLabel>
                   <FieldContent>
-                    <Input {...field} placeholder="Ex: 120,00" />
+                    <Input
+                      {...field}
+                      placeholder="Ex: 120,00"
+                      onChange={changeCurrency}
+                    />
                     {fieldState.invalid && (
                       <FieldError errors={[fieldState.error]} />
                     )}
@@ -60,7 +96,7 @@ export default function DialogService() {
                 </Field>
               )}
             />
-            <p>Tempo de duração do serviço</p>
+            <p className="font-semibold">Tempo de duração do serviço</p>
             <div className="grid grid-cols-2 gap-3">
               <Controller
                 control={form.control}
@@ -105,7 +141,7 @@ export default function DialogService() {
             </div>
           </FieldGroup>
           <Button type="submit" className="w-full font-semibold text-white">
-            teste
+            Adicionar Serviço
           </Button>
         </form>
       </div>
