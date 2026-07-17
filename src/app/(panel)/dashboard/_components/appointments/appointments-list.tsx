5fa485d4c94a77d6b18b3d06b855158ff1501d3e
devlets <leandrovictor2011@gmail.com>
2026-07-17 19:57:28 -0300

    create api appointments

--- src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
+++ src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { useSearchParams } from "next/navigation";
 
 interface AppointmentsListProps {
@@ -8,5 +10,29 @@ export function AppointmentsList({ times }: AppointmentsListProps) {
   const searchParams = useSearchParams();
   const date = searchParams.get("date");
   console.log(date);
-  return <div>teste</div>;
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-xl md:text-2xl font-bold">
+          Agendamentos
+        </CardTitle>
+        <button>Selecionar data</button>
+      </CardHeader>
+      <CardContent>
+        <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4">
+          {times.map((slot) => {
+            return (
+              <div
+                key={slot}
+                className="flex items-center py-2 border-t last:border-b"
+              >
+                <div className="w-16 text-sm font-semibold">{slot}</div>
+                <div className="flex-1 text-sm text-gray-500">Disponível</div>
+              </div>
+            );
+          })}
+        </ScrollArea>
+      </CardContent>
+    </Card>
+  );
 }
