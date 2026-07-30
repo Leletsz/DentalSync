da122fcb847831faecf3593341dcaedb2e20bfa0
devlets <leandrovictor2011@gmail.com>
2026-07-29 23:15:48 -0300

    create grid plans

--- src/app/(panel)/dashboard/plans/_components/grid-plans.tsx
+++ src/app/(panel)/dashboard/plans/_components/grid-plans.tsx
@@ -1,5 +1,46 @@
-import React from "react";
+import { subscriptionPlans } from "@/utils/plans";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export default function GridPlans() {
-  return <article>GridPlans</article>;
+  return (
+    <section className="grid grid-cols-1 gap-3 md:grid-cols-2  md:gap-3">
+      {subscriptionPlans.map((plan, index) => (
+        <Card key={plan.id} className="flex flex-col w-full mx-auto ">
+          {index === 1 && (
+            <div className="bg-emerald-500 w-full py-3 text-center rounded-t-xl -mt-5">
+              <p className="font-semibold text-white">PROMOÇÃO EXCLUSIVA</p>
+            </div>
+          )}
+          <CardHeader>
+            <CardTitle>{plan.name}</CardTitle>
+            <CardDescription>{plan.description}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ul>
+              {plan.features.map((feature, index) => (
+                <li className="text-sm md:text-base" key={index}>
+                  {feature}
+                </li>
+              ))}
+            </ul>
+            <div className="mt-4">
+              <p className="text-gray-600 line-through">{plan.oldPrice}</p>
+              <p className="text-black text-2xl font-bold">{plan.price}</p>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full cursor-pointer">Ativar assinatura</Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </section>
+  );
 }
--- src/app/(panel)/dashboard/plans/page.tsx
+++ src/app/(panel)/dashboard/plans/page.tsx
@@ -1,6 +1,5 @@
 import getSession from "@/lib/getSession";
 import { redirect } from "next/navigation";
-import React from "react";
 import GridPlans from "./_components/grid-plans";
 
 export default function Plans() {
